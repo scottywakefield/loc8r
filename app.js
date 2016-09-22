a54f7fca9c9a1faf10ed5ba210bd90fc5208327d
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2016-09-22 15:06:53 +0100

    added bootstrap and jquery to layout.

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
