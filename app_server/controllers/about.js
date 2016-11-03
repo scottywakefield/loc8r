b513e35bd523b90bb62987b6aa7c666632967c15
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2016-11-03 15:19:02 +0000

    #chpt4 about page moved content out of view.

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(req, res){
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+        title: 'About Loc8r',
+        content: 'Loc8r was created to help people find places to sit down and get some work done, maybe with a coffee, why not?\n\nLorem ipsum blah blah ... stuff.\n\nMore stuff from controller.'
+    });
 };
