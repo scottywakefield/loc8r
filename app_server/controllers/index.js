d4b3b8e24c6653bf8f54ae8bd4581de08707674b
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2016-11-03 11:26:41 +0000

    #chpt4 move data out of index view.

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -1,4 +1,10 @@
 /* GET home page */
 module.exports.index = function(req, res){
-    res.render('index', { title: 'Location list' });
+    res.render('index', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapLine: 'Find places to work near you!'
+        }
+    });
 };
