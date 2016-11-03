f57e224c06faa167e559ac0c494ebb3d71b1de6d
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2016-11-03 15:12:15 +0000

    #chpt4 review content moved out of view.

--- app_server/controllers/review.js
+++ app_server/controllers/review.js
@@ -1,4 +1,7 @@
 /* GET 'review add' page */
 module.exports.add = function (req, res) {
-    res.render('review', {title:'Add a Review'})
-}
+    res.render('review', {
+        title:'Review Scotto\'s Mobos on Loc8r',
+        pageHeader: { title: 'Review Scotto\'s Mobos'}
+    });
+};
