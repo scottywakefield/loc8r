12b3af0e5e0e07e47d42c57680471e545b1e9005
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2016-11-10 10:45:35 +0000

    #chpt5 added db connection handling.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,41 @@
 var mongoose = require("mongoose");
+
+var dbUri = "mongodb://localhost/loc8r";
+mongoose.connect(dbUri);
+
+mongoose.connection.on("connected", function () {
+    console.log("Mongoose connected to " + dbUri);
+});
+
+mongoose.connection.on("error", function (err) {
+    console.log("Mongoose connection error: " + err);
+});
+
+mongoose.connection.on("disconnected", function () {
+    console.log("Mongoose disconnected from " + dbUri);
+});
+
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log("Mongoose disconnected through " + msg);
+        callback();
+    });
+};
+
+process.once("SIGUSR2", function () {
+    gracefulShutdown("nodemon restart", function () {
+        process.kill(process.pid, "SIGUSR2");
+    });
+});
+
+process.on("SIGINT", function () {
+    gracefulShutdown("app termination", function () {
+        process.exit(0);
+    });
+});
+
+process.on("SIGTERM", function () {
+    gracefulShutdown("Heroku app shutdown", function () {
+        process.exit(0);
+    });
+});
