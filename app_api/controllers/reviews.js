233884afeed584d25f6e4257c822b52bbafda29c
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2017-05-04 15:13:59 +0100

    chpt6 added review.add implementation.

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,8 +6,88 @@ var sendJsonResponse = function(res, status, content) {
     res.json(content);
 };
 
+var addReview = function (req, res, location) {
+    if (!location) {
+        sendJsonResponse(res, 404, {"message": "Not found, locationId required"});
+        return;
+    }
+
+    location.reviews.push({
+        author: req.body.author,
+        rating: req.body.rating,
+        reviewText: req.body.reviewText
+    });
+
+    location.save(function (err, location) {
+        var thisReview;
+
+        if (err) {
+            sendJsonResponse(res, 400, err);
+            return;
+        }
+
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length - 1];
+        sendJsonResponse(res, 201, thisReview);
+    });
+};
+
+var updateAverageRating = function (locationId) {
+    Location
+        .findById(locationId)
+        .select("rating reviews")
+        .exec(function (err, location) {
+            if (err) {
+                return;
+            }
+
+            setAverageRating(location);
+        });
+};
+
+var setAverageRating = function (location) {
+    var i, reviewCount, ratingAverage, ratingTotal;
+
+    if (!location.reviews || location.reviews.length === 0) {
+        return;
+    }
+
+    reviewCount = location.reviews.length;
+    ratingTotal = 0;
+    for (i = 0; i < reviewCount; i++) {
+        ratingTotal = ratingTotal + location.reviews[i].rating;
+    }
+    ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+    location.rating = ratingAverage;
+    location
+        .save(function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log("Average rating updated to: " + ratingAverage);
+        })
+};
+
 module.exports.add = function (req, res) {
-    sendJsonResponse(res, 200, {"status": "success"});
+    var locationId = req.params.locationId;
+
+    if (!locationId) {
+        sendJsonResponse(res, 404, {"message": "Not found, locationId required"});
+        return;
+    }
+
+    Location
+        .findById(locationId)
+        .select("reviews")
+        .exec(function (err, location) {
+            if (err) {
+                sendJsonResponse(res, 400, err);
+            } else {
+                addReview(req, res, location);
+            }
+        });
 };
 
 module.exports.get = function (req, res) {
