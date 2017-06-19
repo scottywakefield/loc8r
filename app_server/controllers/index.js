237405e74b03a1191b9d27fe918ed7416d448d6e
Scott Wakefield <scott.wakefield@advancedcomputersoftware.com>
2017-06-19 17:15:14 +0100

    chpt7 index now reads data from api.

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
     apiOptions.server = "https://sw-loc8r.herokuapp.com"
 }
 
-var renderLocations = function (req, res) {
+var renderLocations = function (req, res, responseBody) {
     res.render('index', {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
@@ -18,25 +18,7 @@ var renderLocations = function (req, res) {
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. " +
         "Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-        locations: [{
-            name: 'Starcups',
-            address:'123 Derbs Street, Derby, DE6 6BY',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '25m'
-        },{
-            name: 'Cafe Hero',
-            address:'5 Lichfield Road, Sutton Coldfield, B55 7GG',
-            rating: 4,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            distance: '65m'
-        },{
-            name: 'Burger Queen',
-            address:'3 Station Street, Nottingham, NT6 6BY',
-            rating: 2,
-            facilities: ['Food', 'Premium wifi'],
-            distance: '100m'
-        }]
+        locations: responseBody
     });
 };
 
@@ -49,11 +31,11 @@ module.exports.index = function(req, res){
             qs: {
                 lng: -0.7992599,
                 lat: 51.378091,
-                maxDistance: 20
+                maxDistance: 20000000000
             }
         };
 
     request(requestOptions, function (err, response, body) {
-        renderLocations(req, res);
+        renderLocations(req, res, body);
     });
 };
